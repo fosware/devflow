1b42254baeda9d0f8c2a986e1c3a62886099b811
fosware <fosahadal@protonmail.com>
2024-11-04 17:34:22 -0600

    Assets and Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,9 @@ const spaceGrotesk = localFont({
 
 export const metadata: Metadata = {
   title: "DevFlow",
-  description: "A better version of Stack OverFlow.",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+  icons: "/images/site-logo.svg",
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,6 @@ const Home = () => (
     <h1 className="text-3xl font-black text-dark-500">
       Welcome to the world of Next.js
     </h1>
-    <h1 className="font-space-grotesk text-3xl font-black text-dark-500">
-      Welcome to the world of Next.js (grotesk)
-    </h1>
   </>
 );
 
