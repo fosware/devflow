0d2bbaba64693c19698a3049da980acdfaaf9a09
fosware <fosahadal@protonmail.com>
2024-11-06 17:45:49 -0600

    Set up the theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 const Home = () => (
   <>
-    <h1 className="text-3xl font-black text-dark-500">
-      Welcome to the world of Next.js
-    </h1>
+    <h1 className="h1-bold">Welcome to the world of Next.js</h1>
   </>
 );
 
