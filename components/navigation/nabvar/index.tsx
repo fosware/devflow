3649ec0d56bd5579cf08c29288a002e784afc766
fosa <fosahadal@protonmail.com>
2024-11-07 14:14:05 -0600

    Set up Light & Dark Theme

--- components/navigation/nabvar/index.tsx
+++ components/navigation/nabvar/index.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import Theme from "./Theme";
+
 const Navbar = () => {
   return (
     <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
@@ -16,7 +18,9 @@ const Navbar = () => {
         </p>
       </Link>
       <p>Global Search</p>
-      <div className="flex-between gap-5">Theme</div>
+      <div className="flex-between gap-5">
+        <Theme />
+      </div>
     </nav>
   );
 };
