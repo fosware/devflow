43fd305a4e6ba1bf9abed7edd4ab61fe5984b345
fosa <fosahadal@protonmail.com>
2024-11-12 11:20:53 -0600

    Implement Github SignIn

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,27 +1,23 @@
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
 
-const Home = () => (
-  <>
-    <h1 className="h1-bold">Welcome to the world of Next.js</h1>
-    <DropdownMenu>
-      <DropdownMenuTrigger>Open</DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>Profile</DropdownMenuItem>
-        <DropdownMenuItem>Billing</DropdownMenuItem>
-        <DropdownMenuItem>Team</DropdownMenuItem>
-        <DropdownMenuItem>Subscription</DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  </>
-);
-
+const Home = async () => {
+  const session = await auth();
+  console.log(session);
+  return (
+    <>
+      <h1 className="h1-bold">Welcome to the world of Next.js</h1>
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit">Log out</Button>
+      </form>
+    </>
+  );
+};
 export default Home;
