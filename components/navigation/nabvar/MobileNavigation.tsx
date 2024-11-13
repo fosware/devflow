a89744ae654b5ff20a6cc2d9836980849c9f933d
fosware <fosahadal@protonmail.com>
2024-11-12 18:15:55 -0600

    Mobile Nav & Nav links

--- components/navigation/nabvar/MobileNavigation.tsx
+++ components/navigation/nabvar/MobileNavigation.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import { Button } from "@/components/ui/button";
 import {
   Sheet,
   SheetClose,
@@ -8,6 +9,9 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import ROUTES from "@/constants/routes";
+
+import NavLinks from "./NavLinks";
 
 const MobileNavigation = () => {
   return (
@@ -40,9 +44,25 @@ const MobileNavigation = () => {
         <div className="no-scrollbar flex h-[calc(100vh-80px)] flex-col justify-between overflow-y-auto">
           <SheetClose asChild>
             <section className="flex h-full flex-col gap-6 pt-16">
-              <p>Nav Links</p>
+              <NavLinks isMobileNav={true} />
             </section>
           </SheetClose>
+          <div className="flex flex-col gap-3">
+            <SheetClose asChild>
+              <Link href={ROUTES.SIGN_IN}>
+                <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                  <span className="primary-text-gradient">Log In</span>
+                </Button>
+              </Link>
+            </SheetClose>
+            <SheetClose asChild>
+              <Link href={ROUTES.SIGN_UP}>
+                <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
+                  Sign Up
+                </Button>
+              </Link>
+            </SheetClose>
+          </div>
         </div>
       </SheetContent>
     </Sheet>
